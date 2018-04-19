db919f307118f4406bf9f61f263f1f8f576a2849
Michael Pitz <Michael.Pitz@bmw.de>
2018-04-19 13:18:37 +0200

    first unit test for normal item

--- GildedRoseUnitTests.cc
+++ GildedRoseUnitTests.cc
@@ -2,4 +2,13 @@
 
 #include "GildedRose.h"
 
-TEST(GildedRoseTest, Foo) { EXPECT_TRUE(true); }
+TEST(GildedRoseTest, mormal_item_before_sell_date) {
+  vector<Item> items;
+  int days_remaining{10};
+  int intitial_quality{20};
+  items.push_back(Item("+5 Dexterity Vest", days_remaining, intitial_quality));
+  GildedRose app(items);
+  app.updateQuality();
+  EXPECT_EQ(items[0].days_remaining, days_remaining - 1);
+  EXPECT_EQ(items[0].quality, intitial_quality - 1);
+}
