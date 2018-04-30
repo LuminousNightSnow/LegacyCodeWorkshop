621068ed51959b1f73dcc3644e4fd1fb795d13fa
Michael Pitz <Michael.Pitz@bmw.de>
2018-04-30 09:10:55 +0200

    test fixture for normal items

--- GildedRoseUnitTests.cc
+++ GildedRoseUnitTests.cc
@@ -2,79 +2,73 @@
 
 #include "GildedRose.h"
 
-TEST(GildedRoseTest, normal_item_before_sell_date) {
-  vector<Item> items;
-  const int days_remaining{10};
-  const int intitial_quality{20};
-  items.push_back(Item("+5 Dexterity Vest", days_remaining, intitial_quality));
-  GildedRose app(items);
-  app.updateQuality();
-  EXPECT_EQ(items[0].days_remaining, days_remaining - 1);
-  EXPECT_EQ(items[0].quality, intitial_quality - 1);
+class ItemTest : public testing::Test {
+public:
+  ItemTest(const string &name_) : name_(name_) {}
+
+protected:
+  vector<Item> items_;
+  int days_remaining_{0};
+  int initial_quality_{20};
+  string name_;
+  virtual void MakeAndUpdateItem() = 0;
+};
+
+class NormalItemTest : public ItemTest {
+public:
+  NormalItemTest(const string &name_ = "+5 Dexterity Vest") : ItemTest(name_) {}
+
+protected:
+  void MakeAndUpdateItem() override {
+    items_.push_back(Item(name_, days_remaining_, initial_quality_));
+    GildedRose app(items_);
+    app.updateQuality();
+    EXPECT_EQ(items_[0].days_remaining, days_remaining_ - 1);
+  }
+};
+
+TEST_F(NormalItemTest, before_sell_date) {
+  days_remaining_ = 10;
+  MakeAndUpdateItem();
+  EXPECT_EQ(items_[0].quality, initial_quality_ - 1);
 }
 
-TEST(GildedRoseTest, normal_item_on_sell_date) {
-  vector<Item> items;
-  const int days_remaining{0};
-  const int intitial_quality{10};
-  items.push_back(Item("+5 Dexterity Vest", days_remaining, intitial_quality));
-  GildedRose app(items);
-  app.updateQuality();
-  EXPECT_EQ(items[0].days_remaining, days_remaining - 1);
-  EXPECT_EQ(items[0].quality, intitial_quality - 2);
+TEST_F(NormalItemTest, on_sell_date) {
+  days_remaining_ = 0;
+  MakeAndUpdateItem();
+  EXPECT_EQ(items_[0].quality, initial_quality_ - 2);
 }
 
-TEST(GildedRoseTest, normal_item_after_sell_date) {
-  vector<Item> items;
-  const int days_remaining{-1};
-  const int intitial_quality{8};
-  items.push_back(Item("+5 Dexterity Vest", days_remaining, intitial_quality));
-  GildedRose app(items);
-  app.updateQuality();
-  EXPECT_EQ(items[0].days_remaining, days_remaining - 1);
-  EXPECT_EQ(items[0].quality, intitial_quality - 2);
+TEST_F(NormalItemTest, after_sell_date) {
+  days_remaining_ = -1;
+  MakeAndUpdateItem();
+  EXPECT_EQ(items_[0].quality, initial_quality_ - 2);
 }
 
-TEST(GildedRoseTest, normal_at_zero_quality_after_sell_date) {
-  vector<Item> items;
-  const int days_remaining{-1};
-  const int intitial_quality{0};
-  items.push_back(Item("+5 Dexterity Vest", days_remaining, intitial_quality));
-  GildedRose app(items);
-  app.updateQuality();
-  EXPECT_EQ(items[0].days_remaining, days_remaining - 1);
-  EXPECT_EQ(items[0].quality, intitial_quality);
+TEST_F(NormalItemTest, zero_quality_after_sell_date) {
+  days_remaining_ = -1;
+  initial_quality_ = 0;
+  MakeAndUpdateItem();
+  EXPECT_EQ(items_[0].quality, initial_quality_);
 }
 
-TEST(GildedRoseTest, normal_at_zero_quality_before_sell_date) {
-  vector<Item> items;
-  const int days_remaining{1};
-  const int intitial_quality{0};
-  items.push_back(Item("+5 Dexterity Vest", days_remaining, intitial_quality));
-  GildedRose app(items);
-  app.updateQuality();
-  EXPECT_EQ(items[0].days_remaining, days_remaining - 1);
-  EXPECT_EQ(items[0].quality, intitial_quality);
+TEST_F(NormalItemTest, zero_quality_before_sell_date) {
+  days_remaining_ = 1;
+  initial_quality_ = 0;
+  MakeAndUpdateItem();
+  EXPECT_EQ(items_[0].quality, initial_quality_);
 }
 
-TEST(GildedRoseTest, normal_at_zero_quality_at_sell_date) {
-  vector<Item> items;
-  const int days_remaining{0};
-  const int intitial_quality{0};
-  items.push_back(Item("+5 Dexterity Vest", days_remaining, intitial_quality));
-  GildedRose app(items);
-  app.updateQuality();
-  EXPECT_EQ(items[0].days_remaining, days_remaining - 1);
-  EXPECT_EQ(items[0].quality, intitial_quality);
+TEST_F(NormalItemTest, zero_quality_at_sell_date) {
+  days_remaining_ = 0;
+  initial_quality_ = 0;
+  MakeAndUpdateItem();
+  EXPECT_EQ(items_[0].quality, initial_quality_);
 }
 
-TEST(GildedRoseTest, normal_at_at_sell_date_quality_one) {
-  vector<Item> items;
-  const int days_remaining{0};
-  const int intitial_quality{1};
-  items.push_back(Item("+5 Dexterity Vest", days_remaining, intitial_quality));
-  GildedRose app(items);
-  app.updateQuality();
-  EXPECT_EQ(items[0].days_remaining, days_remaining - 1);
-  EXPECT_EQ(items[0].quality, intitial_quality - 1);
+TEST_F(NormalItemTest, sell_date_quality_one) {
+  days_remaining_ = 0;
+  initial_quality_ = 1;
+  MakeAndUpdateItem();
+  EXPECT_EQ(items_[0].quality, initial_quality_ - 1);
 }
