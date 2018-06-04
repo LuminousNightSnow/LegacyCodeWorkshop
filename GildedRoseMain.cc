faef30c657667a8af76f1247efe3877c3cbfdcb1
Michael Pitz <Michael.Pitz@bmw.de>
2018-06-04 14:32:21 +0200

    added some more items and output

--- GildedRoseMain.cc
+++ GildedRoseMain.cc
@@ -2,12 +2,26 @@
 
 using namespace std;
 
+
 int main() {
-  GildedRose app;
+    GildedRose app;
 
-  app.addItem(Item("+5 Dexterity Vest", 10, 20));
+    app.addItem(Item("+5 Dexterity Vest", 10, 20));
+    app.addItem(Item("Aged Brie", 2, 0));
+    app.addItem(Item("Elixir of the Mongoose", 5, 7));
+    app.addItem(Item("Sulfuras, Hand of Ragnaros", 0, 80));
+    app.addItem(Item("Sulfuras, Hand of Ragnaros", -1, 80));
+    app.addItem(Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+    app.addItem(Item("Backstage passes to a TAFKAL80ETC concert", 10, 49));
+    app.addItem(Item("Backstage passes to a TAFKAL80ETC concert", 5, 49));
 
-  cout << "OMGHAI!" << endl;
-  app.updateQuality();
-  app.printItems();
+    cout << "OMGHAI!" << endl;
+
+    for (int day = 0; day <= 30; day++) {
+        cout << "-------- day " << day << " --------" << endl;
+        app.printItems();
+        cout << endl;
+
+        app.updateQuality();
+    }
 }
